90e2b78c8effa1f033c22711fb35a509c763225e
Snehal Patil <sneha.swapnil1996@gmail.com>
2021-08-25 15:03:05 +0530

    2 problem of function is added

---
+++ palindromefun.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+read -p "Enter the Number" num
+temp=$num
+rev=" "
+function palindrome() {
+        while [ $num -gt 0 ]
+        do
+                a=$(($num%10))
+                num=$(($num/10))
+                rev=$((${rev}${a}))
+        done
+        if [ $temp -eq $rev ]
+        then
+                echo "Number is Palindrome"
+        else
+                echo "Number is Not Palindrome"
+        fi
+}
+palindrome
+
